f6de53c28cfef178b6e81cf942f14f5ff993d125
Mayl Meto <maylme@protonmail.com>
2021-07-12 08:06:51 +0545

    homepage layout began

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp_01/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-          child: Center(
-            child: Container(
-                child: Text('Welcome To Flutter!'),
-              ),
-          ),
-        )
+        home: HomePage()
     );
   }
 }

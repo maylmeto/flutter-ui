a0aa499ef0b1c3d39acde283baf4e748f6531ffd
Mayl Meto <maylme@protonmail.com>
2021-07-12 11:28:46 +0545

    basic routing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp_01/home_page.dart';
+import 'package:myapp_01/pages/home_page.dart';
+import 'package:myapp_01/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomePage()
+        // home: HomePage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.red),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark
+        ),
+        initialRoute:"/home",
+        routes: {
+          "/": (context)=>LoginPage(),
+          "/home":(context) =>HomePage(),
+          "/login":(context) =>LoginPage()
+        },
     );
   }
 }
